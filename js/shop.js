ec5b8ddfc62d32affe420b3eac7690267dd6cf1d
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-05-25 17:43:53 +0200

    Ejercicio 1 - Añadir productos al array de cartList

--- js/shop.js
+++ js/shop.js
@@ -74,7 +74,15 @@ var total = 0;
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    for(let i = 0; i<products.length; i++){
+        if(id == products[i].id){
+            // 2. Add found product to the cartList array
+            cartList.push(products[i]);
+            console.log(cartList);
+            return true;
+        }
+    }
+    return false;
 }
 
 // Exercise 2
