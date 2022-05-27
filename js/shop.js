54479ea6709655dcf113e9c7fed6b3cd5bbdc8e8
Marçal Machado Chaiben <marcal.chaiben@qustodio.com>
2022-05-27 04:23:13 +0200

    Ejercicio 8: Refactoritzación del código - Unificadas las funciones “Buy” y “generareCart” en “addToCart” - Ahora se ejecuta las fuciones de “applyPromotionsCart” y “calculateTotal” solamente cuando se abre el carrito. Antes se las ejecutaba siempre que se añadía un producto al carrito.
    - Arreglado un bug del ejercicio 6 - en el subtotal solamente se enseñaba el producto sin descuento.

--- js/shop.js
+++ js/shop.js
@@ -71,30 +71,30 @@ var cart = [];
 
 var total = 0;
 
-// Exercise 1
-function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    for(let i = 0; i<products.length; i++){
-        if(id == products[i].id){
-            // 2. Add found product to the cartList array
-            cartList.push(products[i]);
-            generateCart();
-            applyPromotionsCart()
-            calculateTotal();
-            printCart()
-            log('Cart List', cartList);
-            return true;
-        }
-    }
-    return false;
-}
-
+// // Exercise 1
+// function buy(id) {
+//     // 1. Loop for to the array products to get the item to add to cart
+//     for(let i = 0; i<products.length; i++){
+//         if(id == products[i].id){
+//             // 2. Add found product to the cartList array
+//             cartList.push(products[i]);
+//             generateCart();
+//             applyPromotionsCart()
+//             calculateTotal();
+//             printCart()
+//             log('Cart List', cartList);
+//             return true;
+//         }
+//     }
+//     return false;
+// }
 // Exercise 2
 function cleanCart() {
     cartList = [];
     cart=[];
     total = 0;
     printCart();
+    document.getElementById('count_product').innerHTML = 0;
     log('Cart List', cartList);
     log('Total', total);
     return true;
@@ -119,34 +119,34 @@ function calculateTotal() {
 }
 
 // Exercise 4
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-    cart = [];
-    for(let i=0; i < cartList.length; i++){
-        let productExists = false;
-        // Check if the element already exist in the cart array
-        for( let j=0; j < cart.length; j++){
-            if(cartList[i].id == cart[j].id){
-                // Product already exists
-                // Increment the quantity value
-                cart[j].quantity++;
-                cart[j].subtotal = Math.round(100*cartList[i].price*cart[j].quantity)/100;
-                productExists = true;
-                break; // Quit this for
-            }
-        }
-        if(productExists == true)
-            continue; // Quit this for
-        // If we didn't find any element we create it
-        let p = cartList[i];
-        p.quantity = 1;
-        p.subtotal = cartList[i].price;
-        cart.push(p);
-    }
-    log('Cart', cart);
-    return true;
-}
+// function generateCart() {
+//     // Using the "cartlist" array that contains all the items in the shopping cart, 
+//     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+//     cart = [];
+//     for(let i=0; i < cartList.length; i++){
+//         let productExists = false;
+//         // Check if the element already exist in the cart array
+//         for( let j=0; j < cart.length; j++){
+//             if(cartList[i].id == cart[j].id){
+//                 // Product already exists
+//                 // Increment the quantity value
+//                 cart[j].quantity++;
+//                 cart[j].subtotal = Math.round(100*cartList[i].price*cart[j].quantity)/100;
+//                 productExists = true;
+//                 break; // Quit this for
+//             }
+//         }
+//         if(productExists == true)
+//             continue; // Quit this for
+//         // If we didn't find any element we create it
+//         let p = cartList[i];
+//         p.quantity = 1;
+//         p.subtotal = cartList[i].price;
+//         cart.push(p);
+//     }
+//     log('Cart', cart);
+//     return true;
+// }
 
 // Exercise 5
 
@@ -164,12 +164,13 @@ function applyPromotionsCart() {
 
 // Exercise 6
 function printHTML(cart){
+    let subtotal = (cart.subtotalWithDiscount !== undefined) ? cart.subtotalWithDiscount : cart.subtotal
     let html = '';
     html += '<tr>';
     html += '    <th scope="row">' + cart.name + '</th>';
     html += '    <td>$' + cart.price + '</td>';
     html += '    <td>' + cart.quantity + '</td>';
-    html += '    <td>$' + cart.subtotal + '</td>';
+    html += '    <td>$' + subtotal + '</td>';
     html += '</tr>';
     return html;
 }
@@ -177,37 +178,64 @@ function printHTML(cart){
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
     let cartHTML = '';
-    let countProduct = 0;
     if (cart.length == 0){
         cartHTML = '<tr><th scope="row" colspan=4 class="text-center pt-5 pb-5" >Empty cart</th></tr>';
     } else {    
         for (let i = 0; i < cart.length; i++) {
             cartHTML = cartHTML + printHTML(cart[i]);
-            countProduct += cart[i].quantity;
         }
     }
     document.getElementById('cart_list').innerHTML = cartHTML;
     document.getElementById('total_price').innerHTML = total;
-    document.getElementById('count_product').innerHTML = countProduct;
 }
 
 
 // ** Nivell II **
 
-// Exercise 7
+// Exercise 8
 function addToCart(id) {
-    // Refactor previous code in order to simplify it 
+    // Flag to check if it is a new or existing product.
+    let productExists = false; 
     // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    for(let i = 0; i<products.length; i++){
+        if(id == products[i].id){
+            // Add product to cartList
+            cartList.push(products[i]);
+            // Update product counter
+            document.getElementById('count_product').innerHTML = cartList.length;
+            // 2. Add found product to the cart array 
+            // or update its quantity in case it has been added previously
+            for(let j=0; j < cart.length; j++){
+                // Existing product
+                if(id == cart[j].id){
+                    // Update quantitiy and subtotal
+                    cart[j].quantity++;
+                    cart[j].subtotal = Math.round(100*cartList[i].price*cart[j].quantity)/100;                   
+                    // Set flag to tell this is an existing product
+                    productExists = true;
+                    break;
+                }
+            }
+            if(productExists == true)
+                continue; 
+            // New product 
+            products[i].quantity = 1;
+            products[i].subtotal = products[i].price;
+            cart.push(products[i]);
+            return true;
+        }
+    }
+    return false;
 }
 
-// Exercise 8
+// Exercise 9
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
 }
 
 function open_modal(){
-	log("Open Modal");
-	printCart();
+    applyPromotionsCart();
+    calculateTotal();
+    printCart();
 }
